65a2b6406067f635eaf9eef769ea60e312c87b43
Kilian Schulte <git@schultek.dev>
2026-04-18 20:54:47 +0200

    update vscode extension

--- modules/jaspr-code/src/constants.ts
+++ modules/jaspr-code/src/constants.ts
@@ -3,4 +3,4 @@ export const isMac = process.platform === "darwin";
 export const isLinux = !isWin && !isMac;
 
 export const JASPR_CREATE_PROJECT_TRIGGER_FILE = 'jaspr.create';
-export const minimumJasprVersion = "0.21.0";
+export const minimumJasprVersion = "0.23.0";
